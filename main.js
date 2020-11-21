5ad2b431d068bb0718b1e0e5eeb9d3d53aa4cf8a
Arrezz <hansarvid96@hotmail.se>
2020-11-21 23:16:02 +0100

    Seeecuuuurity

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 
 const client = new Discord.Client();
 
+let token = ''
 
+fs.readFile('secret.txt', 'utf8', function (err,data) {
+    if (err) {
+        return console.log(err);
+    }
+    token = data;
+});
 
 client.once('ready', () => {
     console.log('Woo boy we starting');
 });
 
-client.login('')
+client.login(token)
